58c147cb8d69ebcbe52825ac77d5473436083d35
Alexey Lushchenko <alexeylush4060@gmail.com>
2022-05-16 09:15:03 +0300

    Implemented Group counting by name

--- server/src/main/java/commands/list/GroupCountingByNameCommand.java
+++ server/src/main/java/commands/list/GroupCountingByNameCommand.java
@@ -2,7 +2,11 @@ package commands.list;
 
 import commands.CommandAbstract;
 import commands.models.CommandFields;
+import models.LabWork;
 import response.Response;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Команда группировки элементов коллекции по значению поля name, вывод количества элементов в каждой группе
@@ -17,25 +21,32 @@ public class GroupCountingByNameCommand extends CommandAbstract {
 
     @Override
     public Response execute(CommandFields commandFields) {
-//        Map<Character, Integer> groupByName = new LinkedHashMap<>();
-//        try{
-//            for (Map.Entry<String, LabWork> entry : commandFields.getLabWorkDAO().getAll().entrySet()) {
-//                Character firstChar = entry.getValue().getName().toCharArray()[0];
-//                if (!groupByName.containsKey(firstChar)){
-//                    groupByName.put(firstChar, 0);
-//                }
-//                groupByName.put(firstChar, groupByName.get(firstChar) + 1);
-//
-//            }
-//
-//            for (Map.Entry<Character, Integer> entry : groupByName.entrySet()) {
-//                commandFields.getConsoleManager().outputln(String.format("Кол-во названий, начинающихся с '%c': %d",entry.getKey(), entry.getValue()));
-//            }
-//
-//            commandFields.getConsoleManager().successfully("Команда group_counting_by_name успешно выполнена");
-//        } catch (NullPointerException nullPointerException){
-//            commandFields.getConsoleManager().error("Ошибка!");
-//        }
-        return null;
+
+        Response response = new Response();
+        response.status = Response.Status.OK;
+        response.type = Response.Type.TEXT;
+        response.command = "group_counting_by_name";
+        Map<Character, Integer> groupByName = new LinkedHashMap<>();
+        try{
+            for (Map.Entry<String, LabWork> entry : commandFields.getLabWorkDAO().getAll().entrySet()) {
+                Character firstChar = entry.getValue().getName().toCharArray()[0];
+                if (!groupByName.containsKey(firstChar)){
+                    groupByName.put(firstChar, 0);
+                }
+                groupByName.put(firstChar, groupByName.get(firstChar) + 1);
+
+            }
+
+            response.argument = "";
+            for (Map.Entry<Character, Integer> entry : groupByName.entrySet()) {
+                response.argument += String.format("Кол-во названий, начинающихся с '%c': %d\n",entry.getKey(), entry.getValue());
+            }
+
+            commandFields.getConsoleManager().successfully("Команда group_counting_by_name успешно выполнена");
+        } catch (NullPointerException nullPointerException){
+            response.status = Response.Status.ERROR;
+            response.message = "Ошибка при выполнение команды!";
+        }
+        return response;
     }
 }
